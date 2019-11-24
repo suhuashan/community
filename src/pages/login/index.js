afff4d7fe2096f90b21b0185b7601584a1b295dd
suhuashan <2891013025@qq.com>
2019-11-24 15:08:23 +0800

    [feature] 完成登录页面的编写以及完成首页部分模块mock数据的测试

---
+++ src/pages/login/index.js
@@ -0,0 +1,75 @@
+import React, { useContext } from 'react';
+import { Form, Icon, Input, Button, message } from 'antd';
+import { LOGIN } from '@/const/api';
+import { AppContext } from '@/App';
+import { ADVANCED_USER, GENERAL_USER } from '@/store/actionType';
+import ajax from '@/util/request';
+import get from 'lodash/get';
+import './index.less';
+
+const AUTH_STATUS = {
+    1: ADVANCED_USER,
+    2: GENERAL_USER
+};
+
+function Login (props) {
+    const { history } = props;
+    const dispatch = useContext(AppContext)[1];
+    const { getFieldDecorator, validateFields } = props.form;
+
+    function handleSubmit (e) {
+        e.preventDefault();
+        validateFields((err, values) => {
+            if (!err) {
+                ajax({
+                    url: LOGIN,
+                    data: values
+                }).then(res => {
+                    let auth = get(res, 'data.auth', 0);
+                    dispatch({type: AUTH_STATUS[auth]});
+                    message.success('登录成功');
+                    history.replace('/home');
+                }).catch(() => {
+                    message.warning('登录失败，请刷新重试');
+                })
+            }
+        });
+    };
+
+    return (
+        <section className="login-wrapper">
+            <Form onSubmit={handleSubmit} className="login-form">
+                <Form.Item>
+                    {getFieldDecorator('username', {
+                        rules: [{ required: true, message: 'Please input your username!' }],
+                    })(
+                        <Input
+                            prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                            placeholder="Username"
+                        />,
+                    )}
+                </Form.Item>
+                <Form.Item>
+                    {getFieldDecorator('password', {
+                        rules: [{ required: true, message: 'Please input your Password!' }],
+                    })(
+                        <Input
+                            prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                            type="password"
+                            placeholder="Password"
+                        />,
+                    )}
+                </Form.Item>
+                <Form.Item>
+                    <Button type="primary" 
+                            htmlType="submit" 
+                            className="login-form-button">
+                        Log in
+                    </Button>
+                </Form.Item>
+            </Form>
+        </section>
+    )
+}
+
+export default Form.create({})(Login);;
