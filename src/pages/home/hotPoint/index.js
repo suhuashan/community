90fb0c56266cdebdfd47a731c876ea766d8b0e9d
suhuashan <2891013025@qq.com>
2019-11-24 15:04:27 +0800

    [feature] 完成首页热门话题模块展示功能

--- src/pages/home/hotPoint/index.js
+++ src/pages/home/hotPoint/index.js
@@ -1,19 +1,58 @@
-import React, { useState } from 'react';
-import { List } from 'antd';
+import React, { useState, useEffect } from 'react';
+import { List, Icon, Tooltip } from 'antd';
 import './index.less';
+import ajax from '@/util/request';
+import { GET_HOT_POINT } from '@/const/api';
+import get from 'lodash/get';
 
 function HotPoint () {
     const [hotList, setHotList] = useState([]);
+
+    /**
+     * @description: 获取热门话题
+     */
+    function getHotPoint (limit, offset) {
+        ajax({
+            url: GET_HOT_POINT,
+            data: {
+                limit,
+                offset
+            }
+        }).then((res) => {
+            setHotList(get(res, 'data.list', []));
+        });
+    }
+
+    /**
+     * @description: 获取更多话题
+     */
+    function getMorePoint () {
+        let hotListLen = hotList.length;
+        getHotPoint(5, hotListLen);
+    }
+
+    useEffect(() => {
+        getHotPoint(5, 0);
+    }, []);
 
     return (
         <section className="hot-point">
             <List
                 size="small"
                 header={<div>热门话题</div>}
-                footer={<div>更多话题</div>}
+                footer={
+                    <div className="more-point" onClick={getMorePoint}>更多话题</div>
+                }
                 bordered
                 dataSource={hotList}
-                renderItem={item => <List.Item>{item}</List.Item>}
+                renderItem={item => 
+                <List.Item>
+                    
+                    <Tooltip placement="topLeft" title={item.title}>
+                        <span className="point-title common-ellipsis">{item.title}</span>
+                    </Tooltip>
+                    <span className="point-views"><Icon type="eye"/>{item.views}</span>
+                </List.Item>}
             />
         </section>
     )
