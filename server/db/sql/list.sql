6faf7ccbd9805d2ef48ff4eda09b00aa437cd503
suhuashan <2891013025@qq.com>
2019-12-02 00:24:59 +0800

    [feature] 修改静态sql文件

---
+++ server/db/sql/list.sql
@@ -0,0 +1,5 @@
+INSERT INTO `list` (`title`, `content`, `views`, `time`, `userId`) VALUES ('明天有人约图书馆吗？', '下午3点到4点左右，南图书馆，可以在天桥下集合,记得千万别迟到，记得千万别迟到，记得千万别迟到，记得千万别迟到', 101, '2019-11-10 22:15:30', 1);
+INSERT INTO `list` (`title`, `content`, `views`, `time`, `userId`) VALUES ('团建报名', '下周末广州从化温泉之都团建，价格为800元/人，想要报名的联系我', 500, '2019-11-10 22:15:30', 2);
+INSERT INTO `list` (`title`, `content`, `views`, `time`, `userId`) VALUES ('想买一本深入浅出Vue.js', '如果有卖此书的人，麻烦联系下我，价格好说', 66, '2019-11-10 22:15:30', 2);
+INSERT INTO `list` (`title`, `content`, `views`, `time`, `userId`) VALUES ('关于webpack实战教程', '慕课网Dell老师的深入浅出webpack系列课程值得学习', 400, '2019-11-10 22:15:30', 1);
+INSERT INTO `list` (`title`, `content`, `views`, `time`, `userId`) VALUES ('react项目实战', 'react实战教程，视频书籍有推荐的吗，好哥哥', 500, '2019-11-10 22:15:30', 1);
--- server/db/sql/user.sql
+++ server/db/sql/user.sql
@@ -1,3 +1,6 @@
-INSERT INTO `user` (`id`, `userName`, `userAge`, `password`) VALUES (2016090207, '小明', 21, '6666666');
-INSERT INTO `user` (`id`, `userName`, `userAge`, `password`) VALUES (2016090208, '小刘', 21, '6666666');
+INSERT INTO `user` (`userName`, `article`, `userAcco`, `userPass`) VALUES ('小明', 10, 'root', '666666');
+INSERT INTO `user` (`userName`, `article`, `userAcco`, `userPass`) VALUES ('小刘', 15, 'admin', '666666');
+INSERT INTO `user` (`userName`, `article`, `userAcco`, `userPass`) VALUES ('小红', 11, 'root1', '666666');
+INSERT INTO `user` (`userName`, `article`, `userAcco`, `userPass`) VALUES ('小亮', 20, 'root2', '666666');
+INSERT INTO `user` (`userName`, `article`, `userAcco`, `userPass`) VALUES ('小兵', 30, 'root3', '666666');
 
