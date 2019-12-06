fd811a69a3fe2a11163af0d4f02ecda8d5248364
suhuashan <2891013025@qq.com>
2019-12-07 00:23:35 +0800

    [feature] 完成首页热门话题和热门作者接口的编写和联调

--- server/controllers/cardList/index.js
+++ server/controllers/cardList/index.js
@@ -18,15 +18,53 @@ const getCardList = async (ctx) => {
                 attributes: []
             }],
             raw:true,
-            offset: Number(offset), 
-            limit: Number(limit)}
-        );
+            offset, 
+            limit
+        });
     ctx.body = new SuccessModel({
         list: cardList,
         total: allList.length
     });
 }
 
+const getHotPoint = async (ctx) => {
+    let { limit, offset } = ctx.request.body;
+    hotPoint = await List.findAll({
+        attributes: [
+            'title', 'views', 'id'
+        ],
+        offset, 
+        limit
+    });
+    ctx.body = new SuccessModel({
+        list: hotPoint
+    });
+}
+
+const getHotWriter = async (ctx) => {
+    let { limit, offset } = ctx.request.body;
+    hotWriter = await List.findAll({
+        attributes: [
+            [Sequelize.col('user.userName'), 'writer'],
+            [Sequelize.col('user.article'), 'article'],
+            'id'
+        ],
+        include: [{
+            model: User,
+            as: 'user',
+            attributes: []
+        }],
+        raw:true,
+        offset, 
+        limit
+    });
+    ctx.body = new SuccessModel({
+        list: hotWriter
+    });
+}
+
 module.exports = {
-    'POST /article/list': getCardList
+    'POST /home/article/list': getCardList,
+    'POST /home/hot/point': getHotPoint,
+    'POST /home/hot/writer': getHotWriter
 }
